59ec7d068b33d91a029b687ad0168600f0ee969f
matufalcon <matiasglf01@gmail.com>
2025-10-04 11:41:39 -0300

    agregue onclcik para seleccionar producto

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function ProductCard({ producto }) { 
+function ProductCard({ producto, onSelect }) { 
   return (
-    <article className="card">
-      <a href={`/productos/${producto.id}`}>
-        <h2>{producto.nombre}</h2>
-        <img src={producto.imagen} alt={producto.nombre} />
-        <p className="precio">${producto.precio.toLocaleString('es-AR')}</p>
-      </a>
+    <article className="card" onClick={onSelect}>
+      
+      <h2>{producto.nombre}</h2>
+      <img src={producto.imagen} alt={producto.nombre} />
+      <p className="precio">${producto.precio.toLocaleString('es-AR')}</p>
+
       <button className="btn-detalle">Añadir al carrito</button>
     </article>
   );
