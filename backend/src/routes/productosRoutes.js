aae8af9e8905540cda8bd2b93d57d5cae046aee2
ramiroorue <orueramiro15@gmail.com>
2025-10-05 22:20:08 -0300

    corrigiendo errores

--- backend/src/routes/productosRoutes.js
+++ backend/src/routes/productosRoutes.js
@@ -13,9 +13,4 @@ router.post('/', controladores.create);
 router.put('/:id', controladores.update);
 router.delete('/:id', controladores.remove);
 
-// Manejador de rutas no válidas dentro de /api/productos
-router.all('*', (req, res) => {
-    res.status(405).json({ message: 'Método no permitido en esta ruta.' });
-});
-
 module.exports = router;
